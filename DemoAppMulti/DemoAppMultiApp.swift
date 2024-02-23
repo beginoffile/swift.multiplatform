b7c04698e7c21b14ca84a7c3cbc8531c7f9159b3
cesar <cursos.mapatupisaro@gamail.com>
2024-02-23 16:41:54 -0600

    Initial Commit

---
+++ DemoAppMulti/DemoAppMultiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoAppMultiApp.swift
+//  DemoAppMulti
+//
+//  Created by Aguid Ramirez Sanchez on 23/02/24.
+//
+
+import SwiftUI
+
+@main
+struct DemoAppMultiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
